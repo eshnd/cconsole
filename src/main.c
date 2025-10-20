f76379e6c03bb95e6fe7cd5610c0da05cb2423a3
eshnd <eshaan2031@icloud.com>
2025-10-19 21:36:28 -0500

    this works.....???

--- src/main.c
+++ src/main.c
@@ -11,35 +11,25 @@ typedef struct {
 char* current_filename;
 
 void exec(char* input){
-  if (strcmp(input, "cshell exit;") == 0){
-    if (access(".cshell_code.c", F_OK) == 0){
-      remove(".cshell_code.c");
-    }
-
-    exit(0);
-  } else if (strcmp(input, "cshell save;") == 0){
-    printf("please provide a filename: ");
-      
-    char* filename;
-    scanf("%s", filename);
-    rename(current_filename, filename);
-
-    return;
-  }
+  
 
   char result[strlen(input) + 2];
   strcpy(result, input);
 
-  result[strlen(input) + 1] = '}';
+  result[strlen(input)] = '}';
+  result[strlen(input) + 1] = '\0';
+
   FILE *code_file = fopen(current_filename, "w");
   fprintf(code_file, "%s", result); 
 
   char command[strlen(current_filename) + 30]; // gcc current_filename -o .cshell_compiled
   sprintf(command, "gcc %s -o .cshell_compiled", current_filename);
 
+  fclose(code_file);
   system(command);
   system("./.cshell_compiled");
   remove(".cshell_compiled");
+
 }
 
 int main(){
@@ -50,13 +40,13 @@ int main(){
   full_code.str = malloc(full_code.size);
   strcpy(full_code.str, "int main(){");
 
+  printf(">>> ");
   while (1){
-    printf("\n>>> ");
-
     dstring to_run;
-    to_run.size = 1;
+    to_run.size = 2;
     to_run.str = malloc(to_run.size);
-    to_run.str[0] = '\0';
+    to_run.str[0] = '\n';
+    to_run.str[1] = '\0';
 
     int input_char = 0;
 
@@ -76,11 +66,27 @@ int main(){
       full_code.size *= 2;
       full_code.str = realloc(full_code.str, full_code.size);
     }
+    
+    if (strcmp(to_run.str, "\ncshell exit;\n") == 0){
+      if (access(".cshell_code.c", F_OK) == 0){
+        remove(".cshell_code.c");
+      }
 
+      exit(0);
+    } else if (strcmp(to_run.str, "\ncshell save;\n") == 0){
+      printf("please provide a filename (less than 50 chars, please): ");
+      
+      char filename[55];
+      scanf("%s", filename);
+      rename(current_filename, filename);
+      continue;
+    }
+    
     strcpy(full_code.str + strlen(full_code.str), to_run.str);
     exec(full_code.str);
 
     free(to_run.str);
+    printf("\n>>> ");
   }
 
   free(full_code.str);
